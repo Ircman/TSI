aaaec01cbbf472282ae53d6df6efc4c05d223a0c
Ircman <33596277+Ircman@users.noreply.github.com>
2017-11-15 09:05:11 +0200

    Update Calculator.hs

--- Calculator.hs
+++ Calculator.hs
@@ -1,20 +1,20 @@
-import Control.Applicative ((<$>), (<*>))
+ import Control.Applicative ((<$>), (<*>))
 type Sign = Double -> Double -> Double
 type Data = (String, Sign)
 type Register = [Data]
-operatorRegister :: Register
-operatorRegister = [("-", (-)),("+", (+)),("/", (/)),("*", (*))]          
+signRegister :: Register
+signRegister = [("-", (-)),("+", (+)),("/", (/)),("*", (*))]          
 main = print $ count "3 * 2 + 5 - 7 / 2"           
 count :: String -> Maybe Double
-count = eval operatorRegister . words          
+count = eval signRegister . words          
 eval :: Register -> [String] -> Maybe Double
 eval [] _ = Nothing 
 eval _ [] = Nothing 
 eval _ [number] = Just $ read number
-eval ((operator, function):rest) unparsed =
-    case span (/=operator) unparsed of
+eval ((signs, function):rest) unparsed =
+    case span (/=signs) unparsed of
         (_, []) -> eval rest unparsed
-        (beforeOperator, afterOperator) -> 
+        (before, after) -> 
             function
-                <$> (eval operatorRegister beforeOperator)
-                <*> (eval operatorRegister $ drop 1 afterOperator)
+                <$> (eval signRegister before)
+                <*> (eval signRegister $ drop 1 after)
